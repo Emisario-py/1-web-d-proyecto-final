ed55b613b082451bc86d58d3714e63c6fd57e824
Emilio <EmilianoR117@outlook.com>
2025-08-19 18:42:24 -0700

    creación proyecto alumnos

---
+++ 3-full-stack/backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import { config } from "dotenv"
+
+config()
+const PORT = process.env.PORT
+const app = express()
+
+app.use( express.json() )
+app.use( cors() )
+
+mongoose.connect(process.env.MONGO_KEY).then(() => console.log("Conectado a MongoDV"))
+
+app.listen( PORT, () => {
+    console.log("Servidor corriendo en el puerto ", PORT)
+})
