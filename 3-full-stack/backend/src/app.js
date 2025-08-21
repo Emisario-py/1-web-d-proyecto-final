ed5f1934688bb2f8ea5ae86ec306f439ba6af7c7
Emilio <EmilianoR117@outlook.com>
2025-08-20 19:42:54 -0700

    rutas, esquema, controller para auth, jwt generado y middleware creado

--- 3-full-stack/backend/src/app.js
+++ 3-full-stack/backend/src/app.js
@@ -3,17 +3,20 @@ import mongoose from "mongoose"
 import cors from "cors"
 import { config } from "dotenv"
 import alumnosRouter from "./routes/alumnos.routes.js"
+import authRoutes from "./routes/auth.routes.js"
 
 config()
+
 const PORT = process.env.PORT
 const app = express()
-app.use( "/api/alumnos", alumnosRouter )
 
 app.use( express.json() )
 app.use( cors() )
+app.use( "/api/alumnos", alumnosRouter )
+app.use( "/api", authRoutes )
 
-mongoose.connect(process.env.MONGO_KEY).then(() => console.log("Conectado a MongoDV"))
+mongoose.connect(process.env.MONGO_KEY).then( () => console.log( "Conectado a MongoDB" ) )
 
-app.listen( PORT, () => {
-    console.log("Servidor corriendo en el puerto ", PORT)
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en el puerto", PORT)
 })
---
+++ 3-full-stack/backend/src/middlewares/validateToken.js
@@ -0,0 +1,5 @@
+
+export const authRequired = (req, res, next) => {
+    console.log(req.headers)
+    next()
+}
